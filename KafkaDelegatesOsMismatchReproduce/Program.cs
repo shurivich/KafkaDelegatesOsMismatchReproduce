ac60238c1ec39c19a4ac8ce76ae3b91973d89622
Alexander Seleznyov <alexanders@oz-code.com>
2021-10-26 17:20:43 +0300

    add repro solution

---
+++ KafkaDelegatesOsMismatchReproduce/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace KafkaDelegatesOsMismatchReproduce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                Console.WriteLine("Succeed to build");
+            }
+
+            Console.WriteLine("We finished");
+        }
+    }
+}
